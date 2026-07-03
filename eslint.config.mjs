57dacd8d6947796f298f7252fd5bade706c738d7
Arkadiusz Szamocki <arkadiusz.szamocki@dynatrace.com>
2026-07-03 14:15:53 +0200

    NOISSUE(chore): add strict tseslint configuration

---
+++ eslint.config.mjs
@@ -0,0 +1,7 @@
+// @ts-check
+
+import js from '@eslint/js';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig([globalIgnores(['dist/**']), js.configs.recommended, ...tseslint.configs.strict]);
