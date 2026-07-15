1feba65c6b2ed0e68445262e1842c7e800ed53ac
Arkadiusz Szamocki <arkadiusz.szamocki@dynatrace.com>
2026-07-15 14:26:27 +0200

    NOISSUE(chore): configure eslint to recognize node environment

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,5 +3,17 @@
 import js from '@eslint/js';
 import { defineConfig, globalIgnores } from 'eslint/config';
 import tseslint from 'typescript-eslint';
+import globals from 'globals';
 
-export default defineConfig([globalIgnores(['dist/**']), js.configs.recommended, ...tseslint.configs.strict]);
+export default defineConfig([
+  globalIgnores(['dist/**']),
+  js.configs.recommended,
+  ...tseslint.configs.strict,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+]);
